9f37a65b66bdcf46c429e222857ec188937607a5
yuvraj <yuvraj.seegolam@umail.uom.ac.mu>
2019-09-07 21:36:00 +0400

    decryption implemented + updated readme

--- app.js
+++ app.js
@@ -9,14 +9,23 @@ const KEY = "HACKERS.MU";
 
 readline.question(`Enter message: `, msg => {
   const cipherText = encryptTxt(msg, KEY);
+  const decryptedText = decryptTxt(cipherText, KEY);
 
   console.log(`KEY: ${KEY}`);
   console.log(`Plain Message: ${msg}`);
   console.log(`Encrypted Message: ${cipherText}`);
+  console.log(`Decrypted Message: ${decryptedText}`);
+  console.log(`Valid: ${decryptedText === msg}`);
 
   readline.close();
 });
 
 function encryptTxt(plainTxt, key) {
   return CryptoJS.AES.encrypt(plainTxt, key).toString();
+}
+
+function decryptTxt(cipherText, key) {
+  const bytes = CryptoJS.AES.decrypt(cipherText, key);
+  const plainTxt = bytes.toString(CryptoJS.enc.Utf8);
+  return plainTxt;
 }
